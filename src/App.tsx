9dc520aca6913067a30eff3843f9247172c4527b
Ove <ove.kristian.jorgensen@gmail.com>
2021-07-06 21:07:35 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Component1 from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Component1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Component1() {
+    return (
+        <div>
+                 
+        </div>
+    )
+}
