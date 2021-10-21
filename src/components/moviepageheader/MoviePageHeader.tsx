d036b2281755e1fcf71b712e502825c8ed28b6c5
Ove Jørgensen <ove.jorgensen@netpower.no>
2021-10-21 10:56:46 +0200

    fixed movie page header

--- src/components/moviepageheader/MoviePageHeader.tsx
+++ src/components/moviepageheader/MoviePageHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
-import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Paper, Theme, Typography } from '@material-ui/core';
 import SearchBar from '../searchbar/SearchBar'
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -32,7 +32,23 @@ const useStyles = makeStyles((theme: Theme) =>
             flexDirection: 'column'
         },
         container: {
-            flexWrap: 'wrap'
+            flexWrap: 'wrap',
+            marginTop: '25px',
+        },
+        paper: {
+            height: '346px',
+            width: '232px',
+            // backgroundColor: 'white',
+            borderRadius: '20px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        thumbnail: {
+            height: '342px',
+            width: '228px',
+            borderRadius: '20px',
+            border: '2px solid white'
         }
     }),
 );
@@ -53,9 +69,26 @@ export default function MoviePageHeader() {
                     <SearchBar homePage={false} />
                 </div>
             </div>
-            <Grid container className={classes.container}>
+            <Grid spacing={4} container className={classes.container}>
                 <Grid item>
-
+                    <Paper elevation={4} className={classes.paper}>
+                        <img className={classes.thumbnail} src='https://s3-alpha-sig.figma.com/img/a23e/1977/b6a19fe8084f1f0165128a4142a8ba00?Expires=1630886400&Signature=HeDgq~LY0V5-SVovs4Yo5CYWjYc5FGYQ-o3eYjvtGM1-LTo5Pxjg3UQ4vFqbGzczaEMqhzMSTVfQiVE8p9IOrv0rI7uTQHLxYYcVfYh~N2l6NxDDAQUM6lPM94roH5T-iLPguGcG9MbP09wbgZSSZk8EO2iVILdbzbNnZXoeSAroaSPx0k7eeRlAgwT1J03hiB4XsPwOixmh1Pq61JbfOva6lsBj-kFwIEwK4QX9InA33Sx~si0lGG5FzQ9WwnAQ4so3uRTjsdssCfd-8v2mNSRG0DEFDbVf1FkKXqRqahd45vGdm-bIVz7XzV81xO9nawSvL2FkWu4~~uunjEk3CA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img>
+                    </Paper>
+                </Grid>
+                <Grid item>
+                    <Paper elevation={4} className={classes.paper}>
+                        <img className={classes.thumbnail} src='https://s3-alpha-sig.figma.com/img/a23e/1977/b6a19fe8084f1f0165128a4142a8ba00?Expires=1630886400&Signature=HeDgq~LY0V5-SVovs4Yo5CYWjYc5FGYQ-o3eYjvtGM1-LTo5Pxjg3UQ4vFqbGzczaEMqhzMSTVfQiVE8p9IOrv0rI7uTQHLxYYcVfYh~N2l6NxDDAQUM6lPM94roH5T-iLPguGcG9MbP09wbgZSSZk8EO2iVILdbzbNnZXoeSAroaSPx0k7eeRlAgwT1J03hiB4XsPwOixmh1Pq61JbfOva6lsBj-kFwIEwK4QX9InA33Sx~si0lGG5FzQ9WwnAQ4so3uRTjsdssCfd-8v2mNSRG0DEFDbVf1FkKXqRqahd45vGdm-bIVz7XzV81xO9nawSvL2FkWu4~~uunjEk3CA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img>
+                    </Paper>
+                </Grid>
+                <Grid item>
+                    <Paper elevation={4} className={classes.paper}>
+                        <img className={classes.thumbnail} src='https://s3-alpha-sig.figma.com/img/a23e/1977/b6a19fe8084f1f0165128a4142a8ba00?Expires=1630886400&Signature=HeDgq~LY0V5-SVovs4Yo5CYWjYc5FGYQ-o3eYjvtGM1-LTo5Pxjg3UQ4vFqbGzczaEMqhzMSTVfQiVE8p9IOrv0rI7uTQHLxYYcVfYh~N2l6NxDDAQUM6lPM94roH5T-iLPguGcG9MbP09wbgZSSZk8EO2iVILdbzbNnZXoeSAroaSPx0k7eeRlAgwT1J03hiB4XsPwOixmh1Pq61JbfOva6lsBj-kFwIEwK4QX9InA33Sx~si0lGG5FzQ9WwnAQ4so3uRTjsdssCfd-8v2mNSRG0DEFDbVf1FkKXqRqahd45vGdm-bIVz7XzV81xO9nawSvL2FkWu4~~uunjEk3CA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img>
+                    </Paper>
+                </Grid>
+                <Grid item>
+                    <Paper elevation={4} className={classes.paper}>
+                        <img className={classes.thumbnail} src='https://s3-alpha-sig.figma.com/img/a23e/1977/b6a19fe8084f1f0165128a4142a8ba00?Expires=1630886400&Signature=HeDgq~LY0V5-SVovs4Yo5CYWjYc5FGYQ-o3eYjvtGM1-LTo5Pxjg3UQ4vFqbGzczaEMqhzMSTVfQiVE8p9IOrv0rI7uTQHLxYYcVfYh~N2l6NxDDAQUM6lPM94roH5T-iLPguGcG9MbP09wbgZSSZk8EO2iVILdbzbNnZXoeSAroaSPx0k7eeRlAgwT1J03hiB4XsPwOixmh1Pq61JbfOva6lsBj-kFwIEwK4QX9InA33Sx~si0lGG5FzQ9WwnAQ4so3uRTjsdssCfd-8v2mNSRG0DEFDbVf1FkKXqRqahd45vGdm-bIVz7XzV81xO9nawSvL2FkWu4~~uunjEk3CA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img>
+                    </Paper>
                 </Grid>
             </Grid>
         </main>
